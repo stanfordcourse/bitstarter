a4c59dc26cff65d4b4cc058f28ff426cd3c1fecd
root <root@Debian-60-squeeze-64-LAMP.(none)>
2013-07-20 20:22:37 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
+  var infile = 'index.html';
   var text = fs.readFileSync(infile);
   response.send(text.toString());
   //response.send('Hello World 2!');
